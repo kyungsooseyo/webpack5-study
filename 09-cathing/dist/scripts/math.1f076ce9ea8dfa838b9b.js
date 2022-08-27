e5fa9d349331e111285f984f1ae52a95a82c4dc5
kyungsooseyo <850767270@qq.com>
2022-08-27 20:25:15 +0800

    feat:对第三方库进行缓存配置

---
+++ 09-cathing/dist/scripts/math.1f076ce9ea8dfa838b9b.js
@@ -0,0 +1,25 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["math"],{
+
+/***/ "./src/math.js":
+/*!*********************!*\
+  !*** ./src/math.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "add": () => (/* binding */ add),
+/* harmony export */   "minus": () => (/* binding */ minus)
+/* harmony export */ });
+var add = function add(x, y) {
+  return x + y;
+};
+var minus = function minus(x, y) {
+  return x - y;
+};
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2NyaXB0cy9tYXRoLjFmMDc2Y2U5ZWE4ZGZhODM4YjliLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7O0FBQU8sSUFBTUEsR0FBRyxHQUFHLFNBQU5BLEdBQU0sQ0FBQ0MsQ0FBRCxFQUFJQyxDQUFKLEVBQVU7RUFDM0IsT0FBT0QsQ0FBQyxHQUFHQyxDQUFYO0FBQ0QsQ0FGTTtBQUdBLElBQU1DLEtBQUssR0FBRyxTQUFSQSxLQUFRLENBQUNGLENBQUQsRUFBSUMsQ0FBSixFQUFVO0VBQzdCLE9BQU9ELENBQUMsR0FBR0MsQ0FBWDtBQUNELENBRk0iLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvbWF0aC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY29uc3QgYWRkID0gKHgsIHkpID0+IHtcclxuICByZXR1cm4geCArIHk7XHJcbn1cclxuZXhwb3J0IGNvbnN0IG1pbnVzID0gKHgsIHkpID0+IHtcclxuICByZXR1cm4geCAtIHk7XHJcbn0iXSwibmFtZXMiOlsiYWRkIiwieCIsInkiLCJtaW51cyJdLCJzb3VyY2VSb290IjoiIn0=
